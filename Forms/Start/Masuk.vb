3178cc3c7e70dea8cca3c75078becdb57e501ae8
AguzzTN54 <gusagusnanang@gmail.com>
2020-07-08 10:23:45 +0700

    Menambah Aset Audio Kls1 dan Kls5-6

--- Forms/Start/Masuk.vb
+++ Forms/Start/Masuk.vb
@@ -10,4 +10,13 @@
         PilihKelas.nama.Text = TextBox1.Text
         PilihKelas.Show()
     End Sub
+
+    Private Sub Masuk_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        Select Case MessageBox.Show("Berhenti Belajar ?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+            Case Windows.Forms.DialogResult.Yes
+            Case Windows.Forms.DialogResult.No
+                e.Cancel = True
+        End Select
+    End Sub
+
 End Class
