4a4db233d0b94125f85f065da5d6ff420ac650f7
verielone <savendavenda@gmail.com>
2020-07-09 20:17:31 +0700

    Fix error Update & Delete, Validasi Login Sudah terdaftar atau Tidak

--- Forms/Start/Daftar.vb
+++ Forms/Start/Daftar.vb
@@ -6,14 +6,35 @@
 		dr = cmd.ExecuteReader
 		dr.Read()
 		If Not dr.HasRows Then
+			Dim id As String
+			Call KoneksiDB()
+			cmd = New OleDb.OleDbCommand("select * from data_siswa order by id", conn)
+			dr = cmd.ExecuteReader
+			dr.Read()
+			If Not dr.HasRows Then
+				id = "SD01"
+			Else
+				id = Val(Microsoft.VisualBasic.Mid(dr.
+				Item("id").ToString, 3, 2)) + 1
+				If Len(id) = 1 Then
+					id = "SD0" & id & ""
+				ElseIf Len(id) = 2 Then
+					id = "SD" & id & ""
+				ElseIf Len(id) = 3 Then
+					id = "S" & id & ""
+				End If
+			End If
+
 			Call KoneksiDB()
 			Dim simpan As String
-			simpan = "insert into data_siswa (nama_siswa)values('" & TextBox1.Text & "')"
+			simpan = "insert into data_siswa (id,nama_siswa)values('" & id & "', '" & TextBox1.Text & "')"
 			cmd = New OleDb.OleDbCommand(simpan, conn)
 			cmd.ExecuteNonQuery()
-            MsgBox("Kamu Sudah Bisa Mulai Belajar Sekarang")
+			MsgBox("Kamu Sudah Bisa Mulai Belajar Sekarang")
+			TextBox1.Text = ""
 		Else
-            MsgBox("Namamu Sudah ada, Kamu Bisa lanjut Belajar")
+			MsgBox("Namamu Sudah ada, Kamu Bisa lanjut Belajar")
+			TextBox1.Text = ""
 		End If
 	End Sub
 
@@ -26,4 +47,8 @@
         Me.Hide()
         Masuk.Show()
     End Sub
+
+	Private Sub Daftar_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+	End Sub
 End Class
--- Module1.vb
+++ Module1.vb
@@ -10,7 +10,7 @@ Module Module1
         Try
             '' MS. Jet OleDB ndak support di laptop aing, tinggal ganti hilangin tanda petik di depan di DB yang mau dipake
 
-            '' conn = New OleDbConnection("provider=microsoft.jet.oledb.4.0; data source=SDApp.accdb")
+            conn = New OleDbConnection("provider=microsoft.jet.oledb.4.0; data source=SDApp.accdb")
             conn = New OleDbConnection("provider=Microsoft.ACE.OLEDB.12.0; data source=../../Resources/database/SDApp.mdb")
 
             conn.Open()
