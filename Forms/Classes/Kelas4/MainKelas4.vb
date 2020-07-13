61a1c023f96193d61f8e28d328be7e3ba9d58bd0
haidimas <dihmaz1234@gmail.com>
2020-07-13 17:55:59 +0700

    materi keals 6

---
+++ Forms/Classes/Kelas4/MainKelas4.vb
@@ -0,0 +1,39 @@
+﻿Public Class MainKelas4
+    Private Sub MainKelas4_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        With Button1
+            .Parent = PictureBox1
+            .FlatStyle = Windows.Forms.FlatStyle.Flat
+            .FlatAppearance.BorderSize = 0
+            .FlatAppearance.MouseDownBackColor = Color.Transparent
+            .FlatAppearance.MouseOverBackColor = Color.Transparent
+            .BackColor = Color.Transparent
+        End With
+
+        With Button2
+            .Parent = PictureBox1
+            .FlatStyle = Windows.Forms.FlatStyle.Flat
+            .FlatAppearance.BorderSize = 0
+            .FlatAppearance.MouseDownBackColor = Color.Transparent
+            .FlatAppearance.MouseOverBackColor = Color.Transparent
+            .BackColor = Color.Transparent
+        End With
+
+        With Button3
+            .Parent = PictureBox1
+            .FlatStyle = Windows.Forms.FlatStyle.Flat
+            .FlatAppearance.BorderSize = 0
+            .FlatAppearance.MouseDownBackColor = Color.Transparent
+            .FlatAppearance.MouseOverBackColor = Color.Transparent
+            .BackColor = Color.Transparent
+        End With
+
+        With Button4
+            .Parent = PictureBox1
+            .FlatStyle = Windows.Forms.FlatStyle.Flat
+            .FlatAppearance.BorderSize = 0
+            .FlatAppearance.MouseDownBackColor = Color.Transparent
+            .FlatAppearance.MouseOverBackColor = Color.Transparent
+            .BackColor = Color.Transparent
+        End With
+    End Sub
+End Class
