627f78d0e9145c81beb59d7ab4ce1cbf9ae854a4
verielone <savendavenda@gmail.com>
2020-07-20 20:37:22 +0700

    Kelas3, Form Kelas5, BG Kelas1

--- Forms/Classes/PilihKelas.vb
+++ Forms/Classes/PilihKelas.vb
@@ -95,10 +95,6 @@
         MainKelas2.Show()
     End Sub
 
-    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-
-    End Sub
-
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
         Me.Hide()
         MainKelas4.Show()
