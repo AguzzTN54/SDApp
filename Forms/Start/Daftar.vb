53fba6598e2e7a4cd993d907f2c121815604fee4
AguzzTN54 <gusagusnanang@gmail.com>
2020-08-04 08:59:21 +0700

    Fix Error Daftar.vb

--- Forms/Start/Daftar.vb
+++ Forms/Start/Daftar.vb
@@ -6,28 +6,14 @@
 		dr = cmd.ExecuteReader
 		dr.Read()
 		If Not dr.HasRows Then
-			Dim id As String
 			Call KoneksiDB()
 			cmd = New OleDb.OleDbCommand("select * from data_siswa order by id", conn)
 			dr = cmd.ExecuteReader
 			dr.Read()
-			If Not dr.HasRows Then
-				id = "SD01"
-			Else
-				id = Val(Microsoft.VisualBasic.Mid(dr.
-				Item("id").ToString, 3, 2)) + 1
-				If Len(id) = 1 Then
-					id = "SD0" & id & ""
-				ElseIf Len(id) = 2 Then
-					id = "SD" & id & ""
-				ElseIf Len(id) = 3 Then
-					id = "S" & id & ""
-				End If
-			End If
 
 			Call KoneksiDB()
 			Dim simpan As String
-			simpan = "insert into data_siswa (id,nama_siswa)values('" & id & "', '" & TextBox1.Text & "')"
+			simpan = "insert into data_siswa (nama_siswa) values('" & TextBox1.Text & "')"
 			cmd = New OleDb.OleDbCommand(simpan, conn)
 			cmd.ExecuteNonQuery()
 			MsgBox("Kamu Sudah Bisa Mulai Belajar Sekarang")
