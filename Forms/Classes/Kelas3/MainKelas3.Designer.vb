b059ee52f6e1d826c9b71f4f871ba0d4f3e1f620
verielone <savendavenda@gmail.com>
2020-08-09 21:50:21 +0700

    Revisi Kelas 3 Semester dihilangkan

--- Forms/Classes/Kelas3/MainKelas3.Designer.vb
+++ Forms/Classes/Kelas3/MainKelas3.Designer.vb
@@ -22,96 +22,157 @@ Partial Class MainKelas3
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainKelas3))
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button4 = New System.Windows.Forms.Button()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.SuspendLayout()
-        '
-        'Button1
-        '
-        Me.Button1.BackColor = System.Drawing.Color.Transparent
-        Me.Button1.BackgroundImage = CType(resources.GetObject("Button1.BackgroundImage"), System.Drawing.Image)
-        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.Button1.Location = New System.Drawing.Point(272, 286)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(149, 153)
-        Me.Button1.TabIndex = 9
-        Me.Button1.UseVisualStyleBackColor = False
-        '
-        'Button2
-        '
-        Me.Button2.BackColor = System.Drawing.Color.Transparent
-        Me.Button2.BackgroundImage = CType(resources.GetObject("Button2.BackgroundImage"), System.Drawing.Image)
-        Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.Button2.Location = New System.Drawing.Point(575, 295)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(149, 153)
-        Me.Button2.TabIndex = 10
-        Me.Button2.UseVisualStyleBackColor = False
-        '
-        'Button3
-        '
-        Me.Button3.BackColor = System.Drawing.Color.Transparent
-        Me.Button3.BackgroundImage = CType(resources.GetObject("Button3.BackgroundImage"), System.Drawing.Image)
-        Me.Button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.Button3.Location = New System.Drawing.Point(735, 543)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(67, 66)
-        Me.Button3.TabIndex = 11
-        Me.Button3.UseVisualStyleBackColor = False
-        '
-        'Button4
-        '
-        Me.Button4.BackColor = System.Drawing.Color.Transparent
-        Me.Button4.BackgroundImage = CType(resources.GetObject("Button4.BackgroundImage"), System.Drawing.Image)
-        Me.Button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.Button4.Location = New System.Drawing.Point(808, 543)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(67, 66)
-        Me.Button4.TabIndex = 12
-        Me.Button4.UseVisualStyleBackColor = False
-        '
-        'PictureBox1
-        '
-        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
-        Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(984, 621)
-        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.PictureBox1.TabIndex = 13
-        Me.PictureBox1.TabStop = False
-        '
-        'MainKelas3
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
-        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.ClientSize = New System.Drawing.Size(984, 621)
-        Me.Controls.Add(Me.Button4)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.PictureBox1)
-        Me.DoubleBuffered = True
-        Me.MaximizeBox = False
-        Me.MinimizeBox = False
-        Me.Name = "MainKelas3"
-        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "MainKelas3"
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.ResumeLayout(False)
+		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainKelas3))
+		Me.Button3 = New System.Windows.Forms.Button()
+		Me.Button4 = New System.Windows.Forms.Button()
+		Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+		Me.Button6 = New System.Windows.Forms.Button()
+		Me.Button5 = New System.Windows.Forms.Button()
+		Me.Button2 = New System.Windows.Forms.Button()
+		Me.Button1 = New System.Windows.Forms.Button()
+		Me.Button7 = New System.Windows.Forms.Button()
+		Me.Button8 = New System.Windows.Forms.Button()
+		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.SuspendLayout()
+		'
+		'Button3
+		'
+		Me.Button3.BackColor = System.Drawing.Color.Transparent
+		Me.Button3.BackgroundImage = CType(resources.GetObject("Button3.BackgroundImage"), System.Drawing.Image)
+		Me.Button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+		Me.Button3.Location = New System.Drawing.Point(735, 543)
+		Me.Button3.Name = "Button3"
+		Me.Button3.Size = New System.Drawing.Size(67, 66)
+		Me.Button3.TabIndex = 11
+		Me.Button3.UseVisualStyleBackColor = False
+		'
+		'Button4
+		'
+		Me.Button4.BackColor = System.Drawing.Color.Transparent
+		Me.Button4.BackgroundImage = CType(resources.GetObject("Button4.BackgroundImage"), System.Drawing.Image)
+		Me.Button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+		Me.Button4.Location = New System.Drawing.Point(808, 543)
+		Me.Button4.Name = "Button4"
+		Me.Button4.Size = New System.Drawing.Size(67, 66)
+		Me.Button4.TabIndex = 12
+		Me.Button4.UseVisualStyleBackColor = False
+		'
+		'PictureBox1
+		'
+		Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+		Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill
+		Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+		Me.PictureBox1.Name = "PictureBox1"
+		Me.PictureBox1.Size = New System.Drawing.Size(984, 621)
+		Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+		Me.PictureBox1.TabIndex = 13
+		Me.PictureBox1.TabStop = False
+		'
+		'Button6
+		'
+		Me.Button6.BackColor = System.Drawing.Color.Transparent
+		Me.Button6.BackgroundImage = CType(resources.GetObject("Button6.BackgroundImage"), System.Drawing.Image)
+		Me.Button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+		Me.Button6.Cursor = System.Windows.Forms.Cursors.Hand
+		Me.Button6.Location = New System.Drawing.Point(376, 287)
+		Me.Button6.Name = "Button6"
+		Me.Button6.Size = New System.Drawing.Size(116, 138)
+		Me.Button6.TabIndex = 21
+		Me.Button6.UseVisualStyleBackColor = False
+		'
+		'Button5
+		'
+		Me.Button5.BackColor = System.Drawing.Color.Transparent
+		Me.Button5.BackgroundImage = CType(resources.GetObject("Button5.BackgroundImage"), System.Drawing.Image)
+		Me.Button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+		Me.Button5.Cursor = System.Windows.Forms.Cursors.Hand
+		Me.Button5.Location = New System.Drawing.Point(224, 287)
+		Me.Button5.Name = "Button5"
+		Me.Button5.Size = New System.Drawing.Size(123, 140)
+		Me.Button5.TabIndex = 20
+		Me.Button5.UseVisualStyleBackColor = False
+		'
+		'Button2
+		'
+		Me.Button2.BackColor = System.Drawing.Color.Transparent
+		Me.Button2.BackgroundImage = CType(resources.GetObject("Button2.BackgroundImage"), System.Drawing.Image)
+		Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+		Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+		Me.Button2.Location = New System.Drawing.Point(67, 287)
+		Me.Button2.Name = "Button2"
+		Me.Button2.Size = New System.Drawing.Size(128, 140)
+		Me.Button2.TabIndex = 19
+		Me.Button2.UseVisualStyleBackColor = False
+		'
+		'Button1
+		'
+		Me.Button1.BackColor = System.Drawing.Color.Transparent
+		Me.Button1.BackgroundImage = CType(resources.GetObject("Button1.BackgroundImage"), System.Drawing.Image)
+		Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+		Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+		Me.Button1.Location = New System.Drawing.Point(779, 288)
+		Me.Button1.Name = "Button1"
+		Me.Button1.Size = New System.Drawing.Size(139, 135)
+		Me.Button1.TabIndex = 24
+		Me.Button1.UseVisualStyleBackColor = False
+		'
+		'Button7
+		'
+		Me.Button7.BackColor = System.Drawing.Color.Transparent
+		Me.Button7.BackgroundImage = CType(resources.GetObject("Button7.BackgroundImage"), System.Drawing.Image)
+		Me.Button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+		Me.Button7.Cursor = System.Windows.Forms.Cursors.Hand
+		Me.Button7.Location = New System.Drawing.Point(637, 287)
+		Me.Button7.Name = "Button7"
+		Me.Button7.Size = New System.Drawing.Size(122, 134)
+		Me.Button7.TabIndex = 23
+		Me.Button7.UseVisualStyleBackColor = False
+		'
+		'Button8
+		'
+		Me.Button8.BackColor = System.Drawing.Color.Transparent
+		Me.Button8.BackgroundImage = CType(resources.GetObject("Button8.BackgroundImage"), System.Drawing.Image)
+		Me.Button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+		Me.Button8.Cursor = System.Windows.Forms.Cursors.Hand
+		Me.Button8.Location = New System.Drawing.Point(502, 288)
+		Me.Button8.Name = "Button8"
+		Me.Button8.Size = New System.Drawing.Size(124, 134)
+		Me.Button8.TabIndex = 22
+		Me.Button8.UseVisualStyleBackColor = False
+		'
+		'MainKelas3
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+		Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+		Me.ClientSize = New System.Drawing.Size(984, 621)
+		Me.Controls.Add(Me.Button1)
+		Me.Controls.Add(Me.Button7)
+		Me.Controls.Add(Me.Button8)
+		Me.Controls.Add(Me.Button6)
+		Me.Controls.Add(Me.Button5)
+		Me.Controls.Add(Me.Button2)
+		Me.Controls.Add(Me.Button4)
+		Me.Controls.Add(Me.Button3)
+		Me.Controls.Add(Me.PictureBox1)
+		Me.DoubleBuffered = True
+		Me.MaximizeBox = False
+		Me.MinimizeBox = False
+		Me.Name = "MainKelas3"
+		Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+		Me.Text = "MainKelas3"
+		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.ResumeLayout(False)
 
-    End Sub
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
+	End Sub
+	Friend WithEvents Button3 As Button
     Friend WithEvents Button4 As Button
     Friend WithEvents PictureBox1 As PictureBox
+	Friend WithEvents Button6 As Button
+	Friend WithEvents Button5 As Button
+	Friend WithEvents Button2 As Button
+	Friend WithEvents Button1 As Button
+	Friend WithEvents Button7 As Button
+	Friend WithEvents Button8 As Button
 End Class
